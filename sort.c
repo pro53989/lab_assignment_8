1dd684bed1ec0efaad2510c4c9b5fb1f5fc8ae9d
pro53989 <122948777+pro53989@users.noreply.github.com>
2023-07-16 19:23:47 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -3,26 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int extraMemoryAllocated;
 
-// implements heap sort
-// extraMemoryAllocated counts bytes of memory allocated
-void heapSort(int arr[], int n)
-{
-for (int i = n / 2 - 1; i >= 0; i--)
-        maxHeapify(arr, n, i);
-
-    // Extract elements from max heap one by one
-    for (int i = n - 1; i >= 0; i--)
-    {
-        // Move current root to end
-        swap(arr, 0, i);
-
-        // call max heapify on the reduced heap
-        maxHeapify(arr, i, 0);
-    }
-}
 void swap(int arr[], int i, int j)
 {
     int temp = arr[i];
@@ -30,172 +12,171 @@ void swap(int arr[], int i, int j)
     arr[j] = temp;
 }
 
-// Helper function to perform max heapify on a subtree rooted at index i in arr[]
 void maxHeapify(int arr[], int n, int i)
 {
-    int largest = i;          // Initialize largest as root
-    int left = 2 * i + 1;     // Left child
-    int right = 2 * i + 2;    // Right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    // If left child is larger than root
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // If right child is larger than largest so far
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // If largest is not root
     if (largest != i)
     {
         swap(arr, i, largest);
-
-        // Recursively heapify the affected sub-tree
         maxHeapify(arr, n, largest);
     }
 }
 
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        maxHeapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr, 0, i);
+        maxHeapify(arr, i, 0);
+    }
+}
+
+void merge(int pData[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = pData[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = pData[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            pData[k++] = L[i++];
+        else
+            pData[k++] = R[j++];
+    }
+
+    while (i < n1)
+        pData[k++] = L[i++];
+
+    while (j < n2)
+        pData[k++] = R[j++];
+
+    free(L);
+    free(R);
+}
+
 void mergeSort(int pData[], int l, int r)
 {
-   int i, j, k;
-	int mid = l + (r - l) / 2;
-	int n1 = mid - l + 1;
-	int n2 = r - mid;
-	int *L = (int *)malloc(n1 * sizeof(int)); // Allocate extra memory for left subarray
-	int *R = (int *)malloc(n2 * sizeof(int)); // Allocate extra memory for right subarray
+    if (l >= r)
+        return;
 
-	if (L == NULL || R == NULL)
-	{
-		printf("Cannot allocate memory\n");
-		exit(-1);
-	}
-
-	for (i = 0; i < n1; i++)
-		L[i] = pData[l + i];
-	for (j = 0; j < n2; j++)
-		R[j] = pData[mid + 1 + j];
-
-	i = 0;
-	j = 0;
-	k = l;
-	while (i < n1 && j < n2)
-	{
-		if (L[i] <= R[j])
-		{
-			pData[k++] = L[i++];
-		}
-		else
-		{
-			pData[k++] = R[j++];
-		}
-	}
-
-	while (i < n1)
-	{
-		pData[k++] = L[i++];
-	}
-
-	while (j < n2)
-	{
-		pData[k++] = R[j++];
-	}
-
-	free(L); // Free allocated memory for left subarray
-	free(R); // Free allocated memory for right subarray
+    int m = l + (r - l) / 2;
+    mergeSort(pData, l, m);
+    mergeSort(pData, m + 1, r);
+    merge(pData, l, m, r);
 }
 
-// parses input file to an integer array
 int parseData(char *inputFileName, int **ppData)
 {
-	FILE* inFile = fopen(inputFileName,"r");
-	int dataSz = 0;
-	int i, n, *data;
-	*ppData = NULL;
+    FILE *inFile = fopen(inputFileName, "r");
+    int dataSz = 0;
+    int i, n, *data;
+    *ppData = NULL;
 
-	if (inFile)
-	{
-		fscanf(inFile,"%d\n",&dataSz);
-		*ppData = (int *)malloc(sizeof(int) * dataSz);
-		// Implement parse data block
-		if (*ppData == NULL)
-		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
-		}
-		for (i=0;i<dataSz;++i)
-		{
-			fscanf(inFile, "%d ",&n);
-			data = *ppData + i;
-			*data = n;
-		}
+    if (inFile)
+    {
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (int *)malloc(sizeof(int) * dataSz);
+        if (*ppData == NULL)
+        {
+            printf("Cannot allocate memory\n");
+            exit(-1);
+        }
+        for (i = 0; i < dataSz; ++i)
+        {
+            fscanf(inFile, "%d ", &n);
+            data = *ppData + i;
+            *data = n;
+        }
 
-		fclose(inFile);
-	}
+        fclose(inFile);
+    }
 
-	return dataSz;
+    return dataSz;
 }
 
-// prints first and last 100 items in the data array
 void printArray(int pData[], int dataSz)
 {
-	int i, sz = dataSz - 100;
-	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
-	{
-		printf("%d ",pData[i]);
-	}
-	printf("\n\t");
+    int i, sz = dataSz - 100;
+    printf("\tData:\n\t");
+    for (i = 0; i < 100; ++i)
+    {
+        printf("%d ", pData[i]);
+    }
+    printf("\n\t");
 
-	for (i=sz;i<dataSz;++i)
-	{
-		printf("%d ",pData[i]);
-	}
-	printf("\n\n");
+    for (i = sz; i < dataSz; ++i)
+    {
+        printf("%d ", pData[i]);
+    }
+    printf("\n\n");
 }
 
 int main(void)
 {
-	clock_t start, end;
-	int i;
+    clock_t start, end;
+    int i;
     double cpu_time_used;
-	char* fileNames[] = { "input1.txt", "input2.txt", "input3.txt", "input4.txt" };
+    char *fileNames[] = {"input1.txt", "input2.txt", "input3.txt", "input4.txt"};
 
-	for (i=0;i<4;++i)
-	{
-		int *pDataSrc, *pDataCopy;
-		int dataSz = parseData(fileNames[i], &pDataSrc);
+    for (i = 0; i < 4; ++i)
+    {
+        int *pDataSrc, *pDataCopy;
+        int dataSz = parseData(fileNames[i], &pDataSrc);
 
-		if (dataSz <= 0)
-			continue;
+        if (dataSz <= 0)
+            continue;
 
-		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
+        pDataCopy = (int *)malloc(sizeof(int) * dataSz);
 
-		printf("---------------------------\n");
-		printf("Dataset Size : %d\n",dataSz);
-		printf("---------------------------\n");
+        printf("---------------------------\n");
+        printf("Dataset Size : %d\n", dataSz);
+        printf("---------------------------\n");
 
-		printf("Heap Sort:\n");
-		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
-		extraMemoryAllocated = 0;
-		start = clock();
-		heapSort(pDataCopy, dataSz);
-		end = clock();
-		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
-		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
-		printArray(pDataCopy, dataSz);
+        printf("Heap Sort:\n");
+        memcpy(pDataCopy, pDataSrc, dataSz * sizeof(int));
+        extraMemoryAllocated = 0;
+        start = clock();
+        heapSort(pDataCopy, dataSz);
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("\truntime\t\t\t: %.1lf\n", cpu_time_used);
+        printf("\textra memory allocated\t: %d\n", extraMemoryAllocated);
+        printArray(pDataCopy, dataSz);
 
-		printf("Merge Sort:\n");
-		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
-		extraMemoryAllocated = 0;
-		start = clock();
-		mergeSort(pDataCopy, 0, dataSz - 1);
-		end = clock();
-		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
-		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+        printf("Merge Sort:\n");
+        memcpy(pDataCopy, pDataSrc, dataSz * sizeof(int));
+        extraMemoryAllocated = 0;
+        start = clock();
+        mergeSort(pDataCopy, 0, dataSz - 1);
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("\truntime\t\t\t: %.1lf\n", cpu_time_used);
+       printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
 
 		free(pDataCopy);
